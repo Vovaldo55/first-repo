4e82e474c8511fb61ca3b0fcf7a6bbcd50009b54
Володимир <15121991vova@gmail.com>
2025-04-07 19:22:52 +0300

    перший коміт

---
+++ Class.main17.pyCh.py
@@ -0,0 +1,15 @@
+def multiply(a,b):return a*b
+def divide(a,b): return a/b
+class calc:pass
+
+
+class Hero:
+    def say_hello(self):
+        print("Привіт! Я герой!")
+
+h = Hero()
+h.say_hello()  # Це викликає метод через об'єкт h
+
+
+
+print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
