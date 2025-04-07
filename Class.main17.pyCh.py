f169049fdf2bca00b5efcfcf5b51636307bcd2d0
Володимир <15121991vova@gmail.com>
2025-04-07 20:16:35 +0300

    перший коміт(зміна 2 рядки)

--- Class.main17.pyCh.py
+++ Class.main17.pyCh.py
@@ -15,10 +15,10 @@ h.say_hello()  # Це викликає метод через об'єкт h
 print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
 print("Hello!")
 print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
-print("Hello!")print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
-print("Hello!")print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
-print("Hello!")print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
-print("Hello!")print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
-print("Hello!")print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
+print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
+print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
+print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
+print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
+print("Hello!") # ПЕРЕВІРКА ДЛЯ Git
 print("Hello!")
 
